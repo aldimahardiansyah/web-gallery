486b9daf5fa1dae02211da1c13480bd7b7c7311a
aldimahardiansyah <aldimdiansyah@gmail.com>
2024-02-07 08:33:12 +0700

    first commit

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,8 @@
+@extends('admin.layout')
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <h3>Selamat datang di halaman dashboard admin.</h3>
+        </div>
+    </div>
+@endsection
