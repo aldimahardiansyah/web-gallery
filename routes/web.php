77f86772c2e0deb8b8c2ccb79dc5767e5d24d9fe
aldimahardiansyah <aldimdiansyah@gmail.com>
2024-02-12 13:16:14 +0700

    buat fitur login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,27 +19,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route untuk menampilkan dashboard admin
-Route::get('/admin', function () {
-    return view('admin.dashboard.index', [
-        'title' => 'Dashboard',
-    ]);
+// Route untuk menampilkan halaman login
+Route::get('/login', [AuthController::class, 'showFormLogin'])->name('login');
+
+// Route untuk menangani proses login
+Route::post('/login', [AuthController::class, 'login']);
+
+// Route untuk pengunjung yang sudah login
+Route::middleware('auth')->group(function () {
+    // Route untuk menampilkan dashboard admin
+    Route::get('/admin', function () {
+        return view('admin.dashboard.index', [
+            'title' => 'Dashboard',
+        ]);
+    });
+
+    // Route untuk menampilkan halaman manajemen admin
+    Route::get('/users', [UserController::class, 'index']);
+
+    // Route untuk menampilkan form tambah admin
+    Route::get('/users/create', [UserController::class, 'create']);
+
+    // Route untuk menyimpan data admin yang baru
+    Route::post('/users', [UserController::class, 'store']);
+
+    // Route untuk menampilkan form edit admin
+    Route::get('/users/{id}/edit', [UserController::class, 'edit']);
+
+    // Route untuk menyimpan update admin
+    Route::put('/users/{id}', [UserController::class, 'update']);
+
+    // Route untuk menghapus data admin
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+    // Route untuk logout
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
-
-// Route untuk menampilkan halaman manajemen admin
-Route::get('/users', [UserController::class, 'index']);
-
-// Route untuk menampilkan form tambah admin
-Route::get('/users/create', [UserController::class, 'create']);
-
-// Route untuk menyimpan data admin yang baru
-Route::post('/users', [UserController::class, 'store']);
-
-// Route untuk menampilkan form edit admin
-Route::get('/users/{id}/edit', [UserController::class, 'edit']);
-
-// Route untuk menyimpan update admin
-Route::put('/users/{id}', [UserController::class, 'update']);
-
-// Route untuk menghapus data admin
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
